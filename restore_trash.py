ff0499675cd0d1f755e85401a59019ace1819dfa
Matt Dewell <mdewell@gmail.com>
2022-08-18 00:40:31 -0500

    logger and runtime adjustments

--- restore_trash.py
+++ restore_trash.py
@@ -1,21 +1,30 @@
+"""restores all items currently in the trash"""
+
 #expects a client_secrets.json file to be downloaded from your google apis 
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
+from loguru import logger
+
 def main():
+    """restores all items currently in the trash"""
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
 
     drive = GoogleDrive(gauth)
 
-    print('getting list')
+    logger.info('getting list')
     file_list = drive.ListFile({'q': "trashed = true"}).GetList()
-    print('have the list')
+    length = len(file_list)
+    logger.info(f'have the list: {length}')
     for file in file_list:
         title = file['title']
         id = file['id']
-        print(f'title: {title}, id: {id}')
+        logger.debug(f'title: {title}, id: {id}')
         file.UnTrash()
+        length -= 1
+        if length % 25 == 0:
+            logger.info(f'remaining: {length}')
 
 if __name__ == "__main__":
     main()
--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import fire
+from loguru import logger
+
 import restore_trash
 
-def restore_trash():
-    print('restoring trash')
-    restore_trash.main()
+class Actions(object):
+    def trash_restore(self):
+        """restores all items currently in the trash"""
+        logger.info('restoring trash')
+        restore_trash.main()
 
-def other():
-    print('other')
+    def other(self):
+        logger.info('other')
 
 if __name__ == '__main__':
-    fire.Fire()
+     fire.Fire(Actions())
